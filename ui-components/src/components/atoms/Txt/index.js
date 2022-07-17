799d4c0353c851c3044222384d1269c9cc832db0
Yuki Kojima <20011472+yuki-kojima@users.noreply.github.com>
2022-07-17 22:30:56 +0900

    create atom - Txt

---
+++ ui-components/src/components/atoms/Txt/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './style.css';
+
+const txtFactory = role => ({ tag:Tag = 'p', size = 'm', className, ...props}) => (
+  <Tag 
+    className={[ styles[role], styles[size], className].join(' ')}
+    { ...props }
+  />
+)
+
+const Txt = txtFactory('default');
+export default Txt;
+
+export const InfoTxt = txtFactory('info');
+export const WarningTxt = txtFactory('warning');
